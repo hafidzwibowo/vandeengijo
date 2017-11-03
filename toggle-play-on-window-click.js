1f1dbc6ec8a2e13ce60db2c9265d90cec8245f0e
Glitch (hello-website) <none>
2017-11-03 03:57:57 +0000

    Checkpoint

---
+++ toggle-play-on-window-click.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent('toggle-play-on-window-click', {
+  init: function () {
+    this.onClick = this.onClick.bind(this);
+  },
+  play: function () {
+    window.addEventListener('click', this.onClick);
+  },
+  pause: function () {
+    window.removeEventListener('click', this.onClick);
+  },
+  onClick: function (evt) {
+    var video = this.el.components.material.material.map.image;
+    video.paused ? video.play() : video.pause();
+  }
+});
